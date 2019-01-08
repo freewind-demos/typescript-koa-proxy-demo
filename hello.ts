e7fffe88dc17cdcad56f1c4fc2a70920c71dd502
freewind <nowindlee@gmail.com>
2019-01-08 14:34:27 +0800

    .

--- hello.ts
+++ hello.ts
@@ -2,9 +2,24 @@ import * as Koa from 'koa';
 
 const app = new Koa();
 
-app.use(ctx => {
-    ctx.body = 'Hello, Koa';
+async function delay(fn: () => void, timeout: number): Promise<void> {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            fn();
+            resolve()
+        }, timeout)
+    })
+}
+
+app.use(async ctx => {
+    const requestTime = Date.now();
+    await delay(() => {
+        const responseTime = Date.now();
+        ctx.body = `Hello, Koa (${requestTime} -> ${responseTime})`;
+    }, 1000);
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('http://localhost:3000');
+});
 
